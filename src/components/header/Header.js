ae6e1135d6f5d77e1d4a9993217d3a9d61503345
DamyanBG <d.dimitrov.1988@abv.bg>
2022-12-11 15:34:17 +0200

    adding some style

---
+++ src/components/header/Header.js
@@ -0,0 +1,17 @@
+import "./Header.scss"
+
+function Header() {
+    return (
+        <header>
+            <h1>Weather Prediction Comparing</h1>
+            <nav>
+                <ul>
+                    <li>Predictions</li>
+                    <li>Sites</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
